20243211afdb4edcb27e16cc83f441a5094d715c
sidharths <sidharthsuresh599@gmail.com>
2023-12-07 17:25:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function fetchWeather() {
+
+    let placeName = inputBox.value;
+    console.log(placeName);
+    let appid = "d28e62f8cc143a8ef0b0c0a612f9bc8f"
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${placeName}&appid=${appid} `
+
+    fetch(url).then(res=>res.json()).then(data=>console.log(data))
+
+    fetch(url).then(res=>res.json()).then(data=>displayWeather(data))
+}
+
+function displayWeather(weather){
+
+    let currentDate = new Date(weather.dt * 1000);
+    let date = currentDate.toDateString(); 
+    let time = currentDate.toLocaleTimeString();
+    let htmlData=`
+        <div class="weather-card border p-4">
+        <h2>${weather.name}</h2>
+        <p>${date} ,${time}</p>
+        <p  class="degree">${weather.main.temp}°C</p>
+        <p>Wind:${weather.wind.speed}m/s  <i class="fa-solid fa-wind"></i></p>
+        <p>${weather.weather[0].description} <i class="fa-solid fa-cloud"></i></p>
+        </div>
+        
+        `
+    document.querySelector("#result").innerHTML=htmlData
+}
+
+
+
+
